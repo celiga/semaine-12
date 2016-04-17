e5819a1b4897da84975d3e2ade9ace764b232607
celiga <celine.garel@yahoo.fr>
2016-04-17 21:05:24 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Semaine 12 Simplon
+Tic Tac Toe en Javascript : [aperçu](http://htmlpreview.github.io/?https://github.com/celiga/semaine-12/blob/master/index.html)
---
+++ script.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+        
+    var previousPlayer = '';
+    var turns = 0;
+    var player = '';
+    var $reset = $('#reset');
+    var squares = $('#game div');
+    var message = $('#message');
+    var attempts = [];
+    var solutions = [
+        ['x1y1','x2y1','x3y1'],
+        ['x1y2','x2y2','x3y2'],
+        ['x1y3','x2y3','x3y3'],
+        ['x1y1','x1y2','x1y3'],
+        ['x2y1','x2y2','x2y3'],
+        ['x3y1','x3y2','x3y3'],        
+        ['x1y1','x2y2','x3y3'],
+        ['x1y3','x2y2','x3y1'],
+    ];
+    
+    function play(e){
+        if(!e.hasClass('nogo')){
+            message.empty().removeClass();
+            player = (turns == 0 || previousPlayer == 2)?'1':'2';
+            //attempts.push(e.attr('id'));
+            previousPlayer = player;
+            turns++;
+            e.addClass('nogo player'+player);
+        }else{
+            message.removeClass().addClass('alert alert-danger').html('Déjà jouée !');
+        }
+    }
+    
+    function reset(){
+        previousPlayer = '';
+        turns = 0;
+        player = '';
+        attempts = [];
+        squares.removeClass();
+    }
+        
+    function check(){
+        attempts = [];    
+        $('.player'+previousPlayer).each(function(i, value){
+            attempts.push($(this).attr('id'));
+        });
+        $.each(solutions, function(i, solution){
+            if($(solution).not(attempts).length == 0){
+                message.removeClass().addClass('alert alert-success').html('Joueur '+previousPlayer+' gagne !');
+                return false;
+            }
+        });
+    }
+    
+    squares.click(function(e){
+        play($(this));
+        if(turns >= 5){
+            check();
+        }
+    });
+    
+    $reset.click(function(){
+        reset();
+    });
+    
+});
